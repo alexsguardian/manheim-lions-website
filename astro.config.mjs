7eb0279a81efd0ef37f0ab46198905b5fdf90960
Alexander Henderson <25626115+alexandzors@users.noreply.github.com>
2026-09-16 16:08:42 -0400

    feat: SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,9 @@ export default defineConfig({
     compress(),
     icon(),
     mdx(),
-    sitemap(),
+    sitemap({
+      filter: (page) => !/\/(form-success|form-error)\/?$/.test(page),
+    }),
     securityTxt({
       contact: 'mailto:webmaster@alexsguardian.net',
     }),
